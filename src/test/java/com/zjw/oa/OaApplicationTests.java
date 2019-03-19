beac055dc1cefc429f3517411d12384daf6492a1
Berserker <dddz97@163.com>
2019-03-19 17:36:09 +0800

    init springboot

---
+++ src/test/java/com/zjw/oa/OaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zjw.oa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
